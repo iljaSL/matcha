efbb192da16ec669c0613f64c0bfba7c4e6fbf4c
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-10-05 15:22:57 +0300

    remove bodyparser as it is unnecessary with current express version https://dev.to/taylorbeeston/you-probably-don-t-need-body-parser-in-your-express-apps-3nio

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import http from 'http';
-import mysql from 'mysql';
 import bodyParser from 'body-parser';
 
 import userController from './controllers/userController.js';
@@ -9,8 +7,6 @@ import userModel from './models/userModel.js';
 import userRoute from './routes/userRoute.js';
 
 const app = express()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api/users/', userRoute);
