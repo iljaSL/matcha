e62520f8b67c398d599270dcfa11077302d0fcfa
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-10-05 10:53:37 +0300

    fix deployment script username

--- backend/config/deploy_db.sh
+++ backend/config/deploy_db.sh
@@ -6,7 +6,7 @@ path=$(cd $( dirname ${BASH_SOURCE[0]}) && pwd )/matcha.sql;
 if [ "$UP" -ne 1 ]; then
      echo "mySQL deamon is not running.";
 else
-    cd /goinfre/ismelich/mamp/mysql/bin;
+    cd /goinfre/$USER/mamp/mysql/bin;
     ./mysql < $path -u root -p;
     echo "Database has been deployed!";
 fi
