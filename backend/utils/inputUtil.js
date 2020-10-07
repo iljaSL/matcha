440c3aa58b4554f92bee3031bd3f9bb6b6168564
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-10-07 17:35:53 +0300

    refactor utils into function expression

--- backend/utils/inputUtil.js
+++ backend/utils/inputUtil.js
@@ -1,18 +1,26 @@
+const realName = (data) => {
+  const regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]+$/u;
+  return !(data == null || data === '' || /\s/.test(data) || !data.match(regex) || data.length < 2 || data.length > 30);
+};
+
+const username = (data) => {
+  const regex = /^[a-zA-Z0-9]*-?[a-zA-Z0-9]*$/;
+  return !(data == null || data === '' || /\s/.test(data) || !data.match(regex) || data.length < 2 || data.length > 30);
+};
+
+const mail = (data) => {
+  const regex = /^([a-zA-Z0-9]+(?:[.\-_]?[a-zA-Z0-9]+)*)@([a-zA-Z0-9]+(?:[.\-_]?[a-zA-Z0-9]+)*)\.([a-zA-Z]{2,})+$/;
+  return !(data == null || data === '' || /\s/.test(data) || !data.match(regex));
+};
+
+const password = (data) => {
+  const regex = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
+  return !(data == null || data === '' || /\s/.test(data) || !data.match(regex));
+};
+
 export default {
-  realName: (data) => {
-    const regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]+$/u;
-    return !(data == null || data === '' || /\s/.test(data) || !data.match(regex) || data.length < 2 || data.length > 30);
-  },
-  username: (data) => {
-    const regex = /^[a-zA-Z0-9]*-?[a-zA-Z0-9]*$/;
-    return !(data == null || data === '' || /\s/.test(data) || !data.match(regex) || data.length < 2 || data.length > 30);
-  },
-  mail: (data) => {
-    const regex = /^([a-zA-Z0-9]+(?:[.\-_]?[a-zA-Z0-9]+)*)@([a-zA-Z0-9]+(?:[.\-_]?[a-zA-Z0-9]+)*)\.([a-zA-Z]{2,})+$/;
-    return !(data == null || data === '' || /\s/.test(data) || !data.match(regex));
-  },
-  password: (data) => {
-    const regex = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
-    return !(data == null || data === '' || /\s/.test(data) || !data.match(regex));
-  },
+  realName,
+  username,
+  mail,
+  password,
 };
