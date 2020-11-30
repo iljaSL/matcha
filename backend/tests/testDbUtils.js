1aa391f67f527af9a59c40f364235036ed1b233b
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-11-30 15:16:21 +0200

    fix truncate table util to postgre

--- backend/tests/testDbUtils.js
+++ backend/tests/testDbUtils.js
@@ -1,21 +1,12 @@
 import pool from '../config/database';
 
 const truncateAllTables = async () => { // super fiddly due to syntax limitations on TRUNCATE
-  await pool.query({
-    sql: 'SET FOREIGN_KEY_CHECKS = 0;',
-  });
-  await pool.query({
-    sql: 'TRUNCATE table usertags;',
-  });
-  await pool.query({
-    sql: 'TRUNCATE table tags;',
-  });
-  await pool.query({
-    sql: 'TRUNCATE table user_photo;',
-  });
-  await pool.query({
-    sql: 'TRUNCATE table users;',
-  });
-};
+  await pool.query('TRUNCATE table usertags CASCADE;');
+  await pool.query('TRUNCATE table tags CASCADE;');
+  await pool.query('TRUNCATE table users CASCADE;');
+  await pool.query('TRUNCATE table user_photo CASCADE;');
+  await pool.query('TRUNCATE table block CASCADE;');
+  await pool.query('TRUNCATE table report CASCADE;');
+}
 
 export default truncateAllTables;
