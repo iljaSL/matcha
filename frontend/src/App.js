b7b5fa5d709650739966af2aff753f2b95cf6b80
iljaSL <iljasmelich@gmail.com>
2020-12-03 17:10:44 +0200

    removed Component folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,10 @@ import {
 } from "react-router-dom";
 
 import './App.css';
-import LoginForm from "./Components/LoginForm/LoginForm";
-import SignUpForm from './Components/SignUp/SignUp'
-import LandingPage from './Components/LandingPage/LandingPage'
-import LandingPagePrompt from "./Components/LandingPage/LandingPagePrompt";
+import LoginForm from "./components/LoginForm/LoginForm";
+import SignUpForm from './components/SignUp/SignUp'
+import LandingPage from './components/LandingPage/LandingPage'
+import LandingPagePrompt from "./components/LandingPage/LandingPagePrompt";
 
 
 const App = () => {
