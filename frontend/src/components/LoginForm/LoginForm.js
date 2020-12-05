63e93a3e8b5ca40c033bcd99222fa24ca79f725c
iljaSL <iljasmelich@gmail.com>
2020-12-05 16:12:40 +0200

    refactored the material-ui imports

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -1,22 +1,24 @@
 import React, { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import {
+    Avatar,
+    Button,
+    CssBaseline,
+    TextField,
+    FormControlLabel,
+    Checkbox,
+    Link,
+    Paper,
+    Grid,
+    CircularProgress,
+    Typography,
+} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Alert from '@material-ui/lab/Alert';
-import Form from "react-validation/build/form";
 
+import Form from "react-validation/build/form";
 import authService from '../../actions/auth';
 
 const useStyles = makeStyles((theme) => ({
