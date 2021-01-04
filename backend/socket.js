384df30e4647151b0c11d23c32f9ea08f8e7dc8f
Esa Halmkrona <ehalmkro@student.hive.fi>
2021-01-04 18:01:36 +0200

    add placeholder socket displaying server time

--- backend/socket.js
+++ backend/socket.js
@@ -1,7 +1,29 @@
 import { Server } from 'socket.io';
 import pool from './config/database.js';
 
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit('FromAPI', response);
+};
+
 export const webSocketServer = (server) => {
-  const io = new Server(server);
-  io.on('connection', () => console.log('user connected'));
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+    },
+  });
+  let interval;
+
+  io.on('connection', (socket) => {
+    console.log('client connected');
+    getApiAndEmit(socket);
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+      clearInterval(interval);
+    });
+  });
+  io.on('sendMessage', (socket) => {
+    console.log('trying to send message...');
+  });
 };
